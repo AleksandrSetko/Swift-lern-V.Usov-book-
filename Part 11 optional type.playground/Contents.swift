4073914bee05950db8af6525aed70266b04cf698
Setko AI <setko@msn.com>
2024-05-29 12:35:25 +0300

    Глава 11 Опциональные типы данных

---
+++ Part 11 optional type.playground/Contents.swift
@@ -0,0 +1,9 @@
+import Cocoa
+
+// nil
+let possibleString = "1945"
+let convertPossibletString = Int(possibleString)
+
+let unpossibleString = "Одна тысяча сто десять"
+let convertUnpossibleString = Int(unpossibleString)
+
